32bb1f6ea3c65fb20c9b5aa9f74bb003835b50b7
Sylvia Mumbi <mumbi099@gmail.com>
2023-03-28 05:00:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
 
-const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 
