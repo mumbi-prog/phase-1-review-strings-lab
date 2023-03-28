6298ae149e0b0e5d4b7cff85453a9d56a9295f8a
Sylvia Mumbi <mumbi099@gmail.com>
2023-03-28 04:55:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Sylvia Mwangi';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
